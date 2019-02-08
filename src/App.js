7b2d464d37b6c915695f85cb489a340e02631f13
John Fleurimond <35304550+JOHNFLEURIMOND@users.noreply.github.com>
2019-02-08 09:58:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Addtodo from './components/Addtodo';
 import Header from './components/Header';
 import axios from 'axios';
 import './App.css';
+import { Animated } from 'react-animated-css';
 
 class App extends Component {
   state = {
@@ -16,7 +17,7 @@ class App extends Component {
       .then(res =>
         this.setState({
           posts: res.data,
-        })
+        }),
       );
   }
 
@@ -25,7 +26,7 @@ class App extends Component {
     axios.delete(`https://jsonplaceholder.typicode.com/posts/${id}`).then(res =>
       this.setState({
         posts: [...this.state.posts.filter(posts => posts.id !== id)],
-      })
+      }),
     );
   };
 
@@ -39,21 +40,24 @@ class App extends Component {
       .then(res =>
         this.setState({
           posts: [...this.state.posts, res.data],
-        })
+        }),
       );
   };
 
   render() {
     return (
-      <div className='App'>
-        <div className='container'>
-          <Header />
-          <Addtodo Addtodo={this.Addtodo} />{' '}
-          <Todo
-            posts={this.state.posts}
-            deleteTodo={this.deleteTodo}
-          />{' '}
-        </div>{' '}
+      <div class="container">
+      <Header />
+        <div class="row">
+          <div class="col-sm-1"></div>
+          <div class="col-sm-10">
+            <Animated animationIn="bounceInLeft" animationOut="fadeOut" isVisible={true}>
+            <Addtodo Addtodo={this.Addtodo} />{' '}
+            <Todo posts={this.state.posts} deleteTodo={this.deleteTodo} />{' '}
+            </Animated>
+          </div>
+          <div class="col-sm-1"></div>
+        </div>
       </div>
     );
   }
